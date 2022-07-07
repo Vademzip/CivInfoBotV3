5a84a618c6c25a4ea2bc3196d61833c84df0fb5a
Vademzip <94627234+Vademzip@users.noreply.github.com>
2022-07-07 14:07:59 +0300

    Загрузка файлов сюда

---
+++ client.js
@@ -0,0 +1,8 @@
+    const Discord = require('discord.js'),
+    config = require('./config.json');
+config.cfg.intents = new Discord.Intents(config.cfg.intents);
+
+const bot = new Discord.Client(config.cfg);
+bot.login(config.token);
+
+require('./events')(bot);
